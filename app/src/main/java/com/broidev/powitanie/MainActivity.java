f03a16ef739cf15750ba15d191eca177b22a5068
Marcel Brodziak <marcelbrodziak@gmail.com>
2026-09-14 12:46:15 +0200

    feat: deklaracja pól i powiązanie widoków w MainActivity

--- app/src/main/java/com/broidev/powitanie/MainActivity.java
+++ app/src/main/java/com/broidev/powitanie/MainActivity.java
@@ -1,6 +1,9 @@
 package com.broidev.powitanie;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // pola klasy - dostępne we wszystkich metodach
+    private EditText etName;
+    private Button btnGreet;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // połączenie pól z elementami layoutu - ZAWSZE po setContentView
+        etName = findViewById(R.id.etName);
+        btnGreet = findViewById(R.id.btnGreet);
+        tvResult = findViewById(R.id.tvResult);
     }
 }
